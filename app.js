af87f530e23bd43fb45de9c7a8fba990e6fcbd7e
James Timmons <james@webmariner.uk>
2016-09-22 00:51:14 +0100

    Attempt at additional waterfall demo

--- app.js
+++ app.js
@@ -29,37 +29,64 @@ server.get('/', restify.serveStatic({
 // Bots Dialogs
 //=========================================================
 
-var intents = new builder.IntentDialog();
+
+
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=6d0966209c6e4f6b835ce34492f3e6d9&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog('/', intents);
 
-intents.matches(/^change name/i, [
+
+intents.matches(/^start again/i, [
     function (session) {
         session.beginDialog('/profile');
-    },
-    function (session, results) {
-        session.send('Ok... Changed your name to %s', session.userData.name);
     }
 ]);
 
 intents.onDefault([
     function (session, args, next) {
-        if (!session.userData.name) {
+        if (!session.userData.age) {
             session.beginDialog('/profile');
         } else {
             next();
         }
     },
     function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+        session.send('Based on your answers, you could get £12345 per year.');
     }
 ]);
 
 bot.dialog('/profile', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        builder.Prompts.text(session, 'Hi! I can help you explore your options at retirement. To get started, can you tell me how old you are?');
     },
     function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
+        session.userData.age = results.response;
+        next();
+        //session.endDialog();
+    },
+    function (session) {
+      builder.Prompts.text(session, 'Next, could you tell me how much your pension pot is worth, in pounds?');
+    },
+    function (session, results) {
+      session.userData.pensionPot = results.response;
+      next();
+    },
+    function (session) {
+      builder.Prompts.text(session, 'How much of this would you like to take out straight away, in pounds?');
+    },
+    function (session, results) {
+      session.userData.lumpSum = results.response;
+      next();
+    },
+    function (session) {
+      builder.Prompts.choice(session, 'Would you like to use the remaining money to buy a guaranteed income for life, or would you prefer more control over the amount of income and how long it lasts?',
+        ['guaranteed', 'flexible']);
+    },
+    function (session, results) {
+      session.userData.choice = results.response;
+      session.send("So you're %s and have £%s in your fund, of which you'd like to take £%s straight away, and use the remaining amount to buy a %s income.",
+        session.userData.age, session.userData.pensionPot, session.userData.lumpSum, session.userData.choice);
     }
 ]);
